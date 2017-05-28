acd4b919adebcbd7b83cd7f922c5ac75299bced8
Alistair Kung <alistaircckung@gmail.com>
2017-05-28 23:04:18 +0100

    step 9,10- interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
+@students= [] #empty array accessable to all methods
+
 def input_students
   puts "Please enter he names of the students"
   puts "To finish, just hit return twice"
-  #create empty array#
-  students= []
   #get the first name
   name = STDIN.gets.gsub("\n",'')
   while !name.empty? do
@@ -11,17 +11,52 @@ def input_students
       if cohort.empty?
         cohort = "unknown"
       end
-    students << {name: name, cohort: cohort}
-    if students.count == 1
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
       word = "student"
     else
       word = "students"
     end
-    puts "Now we have #{students.count} " + word
+    puts "Now we have #{@students.count} " + word
     # get another name from the user
     name = STDIN.gets.gsub("\n",'')
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if @students.count == 0
+    puts "No students were entered."
+  else
+    print_header
+    print_students_list(@students)
+    print_footer(@students)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you nean, try again"
+  end
 end
 
 def print_header
@@ -29,7 +64,7 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print(students)
+def print_students_list(students)
   students.each.with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -39,11 +74,4 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(50)
 end
 
-students = input_students
-if students.count == 0
-  puts "No students were entered."
-else
-  print_header
-  print(students)
-  print_footer(students)
-end
+interactive_menu
