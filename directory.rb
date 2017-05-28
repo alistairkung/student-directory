2ed58d564aae0ce0b4ee7ee247405a588ccdb057
Alistair Kung <alistaircckung@gmail.com>
2017-05-28 20:26:35 +0100

    8.5

--- directory.rb
+++ directory.rb
@@ -1,15 +1,21 @@
 def input_students
   puts "Please enter he names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return three times"
   #create empty array#
   students= []
   #get the first name
   name = gets.chomp
+  puts "Please enter the hobby"
+  hobby = gets.chomp
+  puts "what is his/her height in cm"
+  height = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, hobby: hobby, height: height, cohort: :november}
+    puts "Now we have #{students.count} students. Enter another name or press enter three times to exit."
     # get another name from the user
     name = gets.chomp
+    hobby = gets.chomp
+    height = gets.chomp
   end
   students
 end
@@ -21,7 +27,7 @@ end
 
 def print(students)
   students.each.with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]} height: #{student[:height]}cm hobby: #{student[:hobby]} (#{student[:cohort]} cohort)"
   end
 end
 
